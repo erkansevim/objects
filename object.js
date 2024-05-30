061e0ded396ec659bb2183a8e74918d1059b4918
erkansevim <erkansevim.essgmail.com>
2024-05-30 10:36:22 +0200

    change objects value

--- object.js
+++ object.js
@@ -35,5 +35,53 @@ console.log(person.hasDriverLicense);
 
 //new property add
 
-person.location="Bremen"
+person.location="Bremen";
 console.log(person);
+
+const mensch={
+    name:"adam",
+    surname:"mol",
+    job:"maler",
+    hasDriverLicense:true
+}
+console.log();
+//---------
+
+const people = [
+    {
+      name: "Mustafa",
+      surname: "Gertrud",
+      job: "developer",
+      age: 30,
+    },
+    {
+      name: "Rengin",
+      surname: "Müller",
+      job: "tester",
+      age: 35,
+    },
+    {
+      name: "Mehmet",
+      surname: "Rosenberg",
+      job: "team lead",
+      age: 40,
+    },
+    {
+      name: "Kemal",
+      surname: "Gutenberg",
+      job: "developer",
+      age: 26,
+    },
+  
+    {
+      name: "Halil",
+      surname: "Shaffer",
+      job: "tester",
+      age: 24,
+    }]
+
+    people.forEach((a)=>console.log(a.job));
+
+    const newAges =people.map((kisi)=>kisi.age+1)
+
+    console.log(newAges);
